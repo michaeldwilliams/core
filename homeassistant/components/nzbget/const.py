b8f10205d109c30a1065584f3012b0e56389d0b9
Chris Talkington <chris@talkingtontech.com>
2020-08-16 10:58:21 -0500

    Create const.py

---
+++ homeassistant/components/nzbget/const.py
@@ -0,0 +1,10 @@
+"""Constants for NZBGet."""
+DOMAIN = "nzbget"
+
+# Data
+DATA_UNDO_UPDATE_LISTENER = "undo_update_listener"
+
+# Defaults
+DEFAULT_PORT = 8989
+DEFAULT_SSL = False
+DEFAULT_VERIFY_SSL = False
